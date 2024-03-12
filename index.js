7fcad6e90bac94b852528dfa37f708d119c4fb7b
Eve <>
2024-03-12 14:36:46 +0500

    Add MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
 
 app.get('/', (req,res) => {
-    res.send("Hello from Node API Server");
+    res.send("Hello from Node API Server Updates");
+});
+
+console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("Connected to database!");
+})
+.catch(() => {
+    console.log("Connection failed!");
 });
