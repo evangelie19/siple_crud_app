e24506362fc668fabc8d9d885da3db5d8e11c8c4
Eve <>
2024-03-18 00:02:08 +0500

    Added Product model

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 const express = require('express')
-const app = express()
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const app = express()
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
@@ -10,6 +13,24 @@ app.listen(3000, () => {
 
 app.get('/', (req,res) => {
     res.send("Hello from Node API Server Updates");
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 console.log(process.env.MONGODB_URI);
