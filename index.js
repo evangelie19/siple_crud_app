d9fd7cb7771a074a2f52a8deeef430c67b9451ab
Eve <>
2024-04-28 20:02:50 +0300

    Added files product.controller.js and product.route.js for product routes. This refactoring makes the code more modular and readable, and also simplifies the addition of new routes in the future.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose');
-const Product = require('./models/product.model.js');
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
@@ -12,75 +14,18 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
 
-app.get('/', (req,res) => {
-    res.send("Hello from Node API Server Updates");
+
+// routes
+app.use("/api/products", productRoute);
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
 });
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
 
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-// update a product
-app.put('/api/product/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-
-        const product = await Product.findByIdAndUpdate(id, req.body);
-
-        if (!product) {
-            return res.status(404).json({message: "Product not found"});
-        }
-
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});    
-
-//delete a product
-app.delete ('/api/product/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-
-        const product = await Product.findByIdAndDelete(id);
-
-        if (!product) {
-            return res.status(404).json({message: "Product not found"});
-        }
-
-        res.status(200).json({message: "Product deleted successfully"});
-        
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
+mongoose
 console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
